bfee70e8a124938f5ff0a663838a7b711afee068
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-09-24 23:55:26 +0100

    upd

--- backend/db/initMongoConnection.js
+++ backend/db/initMongoConnection.js
@@ -9,7 +9,7 @@ export const initMongoConnection = async () => {
         const db = env('MONGODB_DB');
 
         await mongoose.connect(
-            `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=AtlasCluster`,
+            `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
         );
         console.log('Mongo connection successfully established!');
     } catch (e) {
